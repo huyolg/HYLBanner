320ddac1e43be4513bab4df48ca73c6502ae3809
Func-Dev-20 (Functional Account) <func-dev-20@signify.com>
2022-12-30 16:52:14 +0800

    modify：first commit

---
+++ HYLBannerView.swift
@@ -0,0 +1,20 @@
+//
+//  HYLBannerView.swift
+//  HYLBanner
+//
+//  Created by 胡永亮 on 2022/12/30.
+//
+
+import UIKit
+
+class HYLBannerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
